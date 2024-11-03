183a332087ab3c82b70e8e8d1ff454e09bb4e6cc
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-11-03 09:21:00 +0100

    feat: implemented cli and added github actions

--- src/generated/automatic_payments/end_timeNone.ts
+++
@@ -1,8 +0,0 @@
-import { Data, mConStr } from "@meshsdk/core";
-
-export interface end_timeNone {
-}
-
-export function end_timeNoneToData(data: end_timeNone): Data {
-    return [];
-}
--- src/generated/automatic_payments/max_payment_delay_hourspayment_interval_hoursend_timeNone.ts
+++
@@ -1,8 +0,0 @@
-import { Data, mConStr } from "@meshsdk/core";
-
-export interface max_payment_delay_hourspayment_interval_hoursend_timeNone {
-}
-
-export function max_payment_delay_hourspayment_interval_hoursend_timeNoneToData(data: max_payment_delay_hourspayment_interval_hoursend_timeNone): Data {
-    return [];
-}
